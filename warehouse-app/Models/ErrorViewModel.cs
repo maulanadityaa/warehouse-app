a76a7fbf934908550729c94e1bf546963b390efd
maulanadityaa <maulanadityaaa@gmail.com>
2024-06-19 19:33:39 +0700

    Initial commit : warehouse-app

---
+++ warehouse-app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace warehouse_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
